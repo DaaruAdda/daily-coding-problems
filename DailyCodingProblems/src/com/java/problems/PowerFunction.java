f258d602c6abafd1aed25f404cfedab5ad9eb38b
Anupam2528 <anupammangale@gmail.com>
2020-10-27 10:45:32 +0530

    Power function is Commited

--- DailyCodingProblems/src/com/java/problems/PowerFunction.java
+++ DailyCodingProblems/src/com/java/problems/PowerFunction.java
@@ -4,6 +4,19 @@ public class PowerFunction {
 	
 	public static void main(String[] args) {
 		
+		System.out.println(power(2, 10));
+		
+	}
+	
+	public static int power(int x, int y){
+		int pow = 1;
+		
+		while(y>=1){
+			pow = pow * x;
+			y--;
+		}
+		
+		return pow;
 	}
 
 }
